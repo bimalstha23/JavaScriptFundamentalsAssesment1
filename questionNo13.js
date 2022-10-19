d337ef06fdcba4bb832586fced91feb473c8a855
Bimal Shrestha <bimalshrestha074@gmail.com>
2022-10-19 12:52:45 +0545

    convert a string into uppercase or lowercase according to the condition[Qno13]

---
+++ questionNo13.js
@@ -0,0 +1,9 @@
+function convert(string){
+    if(string.length<=3){
+        return string.toUpperCase();
+    }else{
+        return string.slice(0,3).toLowerCase()+string.slice(3,string.length); //slice method will return the string from 0 to 3 index and convert it into lower case and add the rest of the string
+    }
+}
+
+console.log(convert('hi'))
