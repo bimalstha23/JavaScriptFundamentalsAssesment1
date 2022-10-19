b8980e2f012267f4bf5800581c44a57adafce96b
Bimal Shrestha <bimalshrestha074@gmail.com>
2022-10-19 13:07:44 +0545

    uppercase vowels added in vowels array list

--- questionNo10.js
+++ questionNo10.js
@@ -1,5 +1,5 @@
 function coutVowels(string){
-    const vowels = ['a','e','i','o','u'] 
+    const vowels = ['a','e','i','o','u','A','E','I','O','U'] 
     let count = 0;
     for(i=0;i<string.length;i++){
         if(vowels.includes(string[i])) count++ //returs true and increase the count by 1 if it finds the letter in vowels array else false and does nout increase the count
