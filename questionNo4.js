34a475ad296cbf44d296fa0c7e8dd6804294a03d
Bimal Shrestha <bimalshrestha074@gmail.com>
2022-10-19 09:30:44 +0545

    properties of object in js[question no 4]

---
+++ questionNo4.js
@@ -0,0 +1,10 @@
+
+function getObjectProporties(obj) {
+    return Object.getOwnPropertyNames(obj).filter( (prop)=> {
+        return typeof obj[prop] === 'function' //returns only functions of an object
+    });
+}
+
+const proporties = getObjectProporties(Math);
+console.log(proporties);
+console.log(Object.getOwnPropertyNames(Math)); //returns everything including functions and variables
